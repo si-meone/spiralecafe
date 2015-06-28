3bdc24be0af670c0a37b0cdce1b9955ae681db3a
Nasrallah <simon_nasrallah@hotmail.com>
2015-06-28 16:25:14 +0100

    removed local host

--- guestbook/guestbook.py
+++ guestbook/guestbook.py
@@ -57,7 +57,7 @@ class MainPage(webapp2.RequestHandler):
             'url_linktext': url_linktext,
             }
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('index2.html')
         self.response.write(template.render(template_values))
 
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+latest to deploy go into project folder and type
+appcfg.py update . --no_cookies
